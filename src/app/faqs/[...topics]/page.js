47362e36a00003e00eaf1b2dbfc4a46352063756
devsrsihab <srsohankhan0@gmail.com>
2024-01-19 15:33:44 +0600

    Catch all segment routing

--- src/app/faqs/[...topics]/page.js
+++ src/app/faqs/[...topics]/page.js
@@ -1,9 +1,10 @@
-const PricingPerPage = ({params}) => {
+
+const Topics = ({params}) => {
     return (
         <h1 className="font-[900] text-center text-3xl capitalize text-gray-900 lg:text-5xl lg:leading-snug">
-        Dynamic Routing price: {params.perpricing}
+        Cach All Segemnt Routing : {params.topics}
       </h1>
     );
 };
 
-export default PricingPerPage;
+export default Topics;

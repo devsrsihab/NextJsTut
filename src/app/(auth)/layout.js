c6a1f48b4da31249934c014401c4899a1f00d0e5
devsrsihab <srsohankhan0@gmail.com>
2024-01-27 13:52:44 +0600

    nested layout completed

---
+++ src/app/(auth)/layout.js
@@ -0,0 +1,11 @@
+
+const AuthLayout = ({children}) => {
+    return (
+        <div>
+            <h2 className="text-3xl text-red-500 text-center font-bold">Auth Layout</h2>
+            {children}
+        </div>
+    );
+};
+
+export default AuthLayout;
